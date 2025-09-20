4aa6ff206d8d8aa1593deacf0b00667e9516032c
AshwinGour28 <ashwing2884@gmail.com>
2025-09-20 14:14:24 +0530

    created mvc for user

--- journalApp/src/main/java/com/project/journalApp/entity/JournalEntry.java
+++ journalApp/src/main/java/com/project/journalApp/entity/JournalEntry.java
@@ -3,6 +3,7 @@ package com.project.journalApp.entity;
 import java.time.LocalDateTime;
 
 import lombok.Data;
+import lombok.NonNull;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -13,6 +14,7 @@ public class JournalEntry {
 
     @Id
     private ObjectId id;
+    @NonNull
     private String title;
     private String content;
     private LocalDateTime date;
