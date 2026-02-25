9b79880c27f2fca060605c9112ce14d2643d7690
AshwinGour28 <ashwing2884@gmail.com>
2026-02-25 19:01:19 +0530

    added spring security

--- journalApp/src/main/java/com/project/journalApp/controller/UserController.java
+++ journalApp/src/main/java/com/project/journalApp/controller/UserController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,24 +19,22 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @GetMapping
-    public List<User> getAllUsers(){
-        return userService.getAll();
-    }
 
-    @PostMapping
-    public void createUser(@RequestBody User user){
-        userService.saveEntry(user);
-    }
-
-    @PutMapping("/{username}")
-    public ResponseEntity<?> updateUser(@RequestBody User user, @PathVariable String username){
+    @PutMapping
+    public ResponseEntity<?> updateUser(@RequestBody User user){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
         User userInDB = userService.findByUsername(username);
-        if(userInDB!=null){
-            userInDB.setUsername(user.getUsername());
-            userInDB.setPassword(user.getPassword());
-            userService.saveEntry(userInDB);
-        }
+        userInDB.setUsername(user.getUsername());
+        userInDB.setPassword(user.getPassword());
+        userService.saveNewEntry(userInDB);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<?> deleteUser(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        userService.deleteByUsername(authentication.getName());
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
--- journalApp/src/main/java/com/project/journalApp/service/UserService.java
+++ journalApp/src/main/java/com/project/journalApp/service/UserService.java
@@ -4,8 +4,11 @@ import com.project.journalApp.entity.User;
 import com.project.journalApp.repository.UserRepository;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -15,7 +18,15 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    private static final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
     public void saveEntry(User user){
+        userRepository.save(user);
+    }
+
+    public void saveNewEntry(User user){
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setRoles(Arrays.asList("USER"));
         userRepository.save(user);
     }
 
@@ -24,6 +35,8 @@ public class UserService {
     public Optional<User> findById(ObjectId id){ return userRepository.findById(id);}
 
     public void deleteById(ObjectId id){ userRepository.deleteById(id);}
+
+    public void deleteByUsername(String username){userRepository.deleteByUsername(username);};
 
     public User findByUsername(String username){
         return userRepository.findByUsername(username);
